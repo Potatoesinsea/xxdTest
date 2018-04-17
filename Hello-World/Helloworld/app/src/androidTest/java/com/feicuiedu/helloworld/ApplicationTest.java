75bbe0c94348619f1d282692e29809750bf0a8fb
xxd <1261375515@qq.com>
2018-04-17 09:35:40 +0800

    提交Helloworld的一个测试类

--- Hello-World/Helloworld/app/src/androidTest/java/com/feicuiedu/helloworld/ApplicationTest.java
+++ Hello-World/Helloworld/app/src/androidTest/java/com/feicuiedu/helloworld/ApplicationTest.java
@@ -5,7 +5,7 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+ */哈哈哈哈哈哈哈啊哈哈哈哈哈哈
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
